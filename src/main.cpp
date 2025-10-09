d7cf8ef4386d87191e9443675d4d0dbff9d26837
Ayushman-Choudhuri <ayushc205@gmail.com>
2025-10-09 09:05:22 +0200

    fix: 1. removed initlization execution in the main function 2. updated default camera ID

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,20 @@
-#include "camera.hpp"
-#include <opencv2/core/utils/logger.hpp>
 #include <iostream>
+#include <opencv2/core/utils/logger.hpp>
 
-#define DEFAULT_DEVICE_ID 0
+#include "camera.hpp"
+
+#define DEFAULT_DEVICE_ID 4
 
 int main(int argc, char** argv) {
     cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_SILENT);
-        
-    
+
     std::cout << "Using camera device ID: " << DEFAULT_DEVICE_ID << std::endl;
-    
-    
+
     Camera webcam(DEFAULT_DEVICE_ID);
-    
-    if (!webcam.initialize()) {
-        std::cerr << "Failed to initialize camera!" << std::endl;
-        return -1;
-    }
-    
-    webcam.run("My Webcam Feed");
-    
-    std::cout << "Application terminated successfully." << std::endl;
+
+    // cv::Mat frame = webcam.getFrame();
+
+    webcam.visualize("Camera feed");
+    // std::cout << "Application terminated successfully." << std::endl;
     return 0;
 }
